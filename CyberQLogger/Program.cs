c5a10c7d4e1c52350c2a7509061c745471420377
Chris Mallery <chmaller@microsoft.com>
2017-04-13 13:38:54 -0700

    Add project files.

---
+++ CyberQLogger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberQLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
